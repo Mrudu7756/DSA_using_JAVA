4ceef4a4b2d0346bd89e1dcc0c572750e95ff5ef
Mrudula Rajendra Tandel <163021229+Mrudu7756@users.noreply.github.com>
2026-07-05 23:16:26 +0530

    Add files via upload

---
+++ FourDirectionMazePath.java
@@ -0,0 +1,25 @@
+public class FourDirectionMazePath {
+    public static void main(String[] args) {
+        int rows = 3;
+        int cols = 3;
+        print(1,1,rows,cols,"");    
+    }
+    private static void print(int sr, int sc, int er, int ec, String s) {
+        if(sr<1 || sc<1) return;
+        if(sr > er||sc > ec){
+            return;
+        }
+        if(sc==ec && sr==er){
+            System.out.println(s);
+            return;
+        }
+        //go down
+        print(sr+1, sc, er, ec, s+"D");
+        // go right
+        print(sr, sc+1, er, ec, s+"R");
+        // go left
+        print(sr, sc-1, er, ec, s+"L");
+        // go up
+        print(sr-1, sc, er, ec, s+"U");
+    }
+}
